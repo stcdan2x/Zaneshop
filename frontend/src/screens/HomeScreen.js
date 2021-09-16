b925cecf013e3a808ecfc7c536134fbfb2430447
Steven Dandan <stcdan2x@gmail.com>
2021-09-17 03:43:05 +0800

    completed implementing pagination in homescreen and search results. created paginate.js and used react-bs5 pagination component for creating own pagination component, modified productcontroller.js to process more data in the backend and send additional data to frontend, modified listproducts. modified app.js to render pages that includes pagination feature. added pagination component to homescreen.js.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,36 +5,40 @@ import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
+import Paginate from "../components/Paginate";
 
 
 const HomeScreen = (props) => {
    const dispatch = useDispatch();
 
-   const keyword = props.match.params.keyword
-   const pageNumber = props.match.params.pageNumber
+   const keyword = props.match.params.keyword;
+   const pageNumber = props.match.params.pageNumber || 1;
 
    const productList = useSelector(state => state.productList);
-   const { products, loading, error } = productList;
+   const { products, page, pages, loading, error } = productList;
 
    useEffect( () => {
-      dispatch( listProducts(keyword) )
-   }, [dispatch, keyword]);
+      dispatch( listProducts(keyword, pageNumber) )
+   }, [dispatch, keyword, pageNumber]);
 
    return (
       <>
          <h1>Latest Products</h1>
          {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> :
          (
-            <Row>
-               {products.map( product => (
-                  <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
-                     <Product product={product} />
-                  </Col>
-               ))}
-            </Row>
+            <>
+               <Row>
+                  {products.map( product => (
+                     <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
+                        <Product product={product} />
+                     </Col>
+                  ))}
+               </Row>
+               <Paginate page={page} pages={pages} keyword={keyword ? keyword : ""} />
+            </>
          )}
       </>
    )
 }
 
-export default HomeScreen
+export default HomeScreen;
